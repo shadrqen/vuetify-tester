e383acfca6690025bcde6f9ec81a1a412b85db8e
shadrqen <shadrqen@outlook.com>
2022-03-08 16:22:31 +0300

    feat: add base tester component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <BaseTester></BaseTester>
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data () {
-    return {
-      //
-    }
-  },
-})
+<script setup lang="ts">
+import BaseTester from './components/BaseTester.vue'
 </script>
